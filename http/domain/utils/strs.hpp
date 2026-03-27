6fa8f8690725d9c3643baa5574723bb4e70d2d29
Zed <skhull.coding@gmail.com>
2026-03-27 11:05:52 +0530

    feat: lower a string via view

--- http/domain/utils/strs.hpp
+++ http/domain/utils/strs.hpp
@@ -6,4 +6,15 @@
 typedef std::string str;
 typedef std::string_view strv;
 
+/// @brief Return the lowercased version of s
+/// @param s string view s
+/// @return a lowercased string version of s
+str tolower(strv s)
+{
+    str lowercased(s);
+    transform(lowercased.begin(), lowercased.end(), lowercased.begin(), [](unsigned char c)
+              { return tolower(c); });
+    return lowercased;
+}
+
 #endif
