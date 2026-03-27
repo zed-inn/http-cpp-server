4904214165ff3b21c2836e9801639314aa114f1e
Zed <skhull.coding@gmail.com>
2026-03-27 11:06:23 +0530

    feat: check if characters are in certain char formats

---
+++ http/domain/utils/whitespaces.hpp
@@ -0,0 +1,19 @@
+#ifndef _C_HTTP_UTILS_WHITESPACE_CHARACTERS_
+#define _C_HTTP_UTILS_WHITESPACE_CHARACTERS_ 1
+
+// Whitespace
+#define isWS(x) (x == ' ' || x == '\t')
+
+// Carriage return
+#define isCR(x) (x == ' \r')
+
+// Line feed
+#define isLF(x) (x == '\n')
+
+// Carriage return and line feed
+#define isCRLF(x, y) (isCR(x) && isLF(y))
+
+// CRLF twice
+#define is2xCRLF(w, x, y, z) (isCRLF(w, x) && isCRLF(y, z))
+
+#endif
